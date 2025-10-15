6c06bcb02131d3726231e325348667bc30490657
Aluno SENAI <Aluno@SN-396858.edu.fiesc.com.br>
2025-10-15 17:47:08 -0300

    Att_3

--- server.js
+++ server.js
@@ -27,15 +27,49 @@ server.get('/usuarios', async (req, reply) => {
 
 server.post('/usuarios', async (req, reply) => {
     const { nome, senha, email, telefone } = req.body
+
     try {
-        const resultado = await pool.query('INSERT INTO USUARIO (NOME, SENHA, EMAIL, TELEFONE) VALUES ($1, $2, $3, $4) RETURNING *', [nome, senha, email, telefone])
+        const resultado = await pool.query(
+            'INSERT INTO USUARIO (NOME, SENHA, EMAIL, TELEFONE) VALUES ($1, $2, $3, $4) RETURNING *',
+            [nome, senha, email, telefone]
+        )
         reply.status(200).send(resultado.rows[0])
-    }catch(e){
+    } catch(e) {
         reply.status(500).send({ error: e.message })
     }
 })
 
+// deletar
 
+server.delete('/usuarios/:id', async (req, reply) => {
+    const id = req.params.id
+
+    try {
+        await pool.query('DELETE FROM USUARIO WHERE id=$1', [id])
+        reply.send({ message: 'Usuário deletado' })
+    } catch(e) {
+        reply.send({ error: e.message })
+    }
+})
+
+// editar
+
+server.put('/usuarios/:id', async (req, reply) => {
+    const { nome, senha, email, telefone } = req.body
+    const id = req.params.id
+
+    try {
+        const resultado = await pool.query(
+            'UPDATE USUARIO SET nome=$1, senha=$2, email=$3, telefone=$4 WHERE id=$5 RETURNING *',
+            [nome, senha, email, telefone, id]
+        )
+        reply.status(200).send(resultado.rows[0])
+    } catch(e) {
+        reply.status(500).send({ error: e.message })
+    }
+})
+
+// categorias
 
 server.get('/categorias', async (req, reply) => {
     try {
@@ -53,6 +87,36 @@ server.post('/categorias', async (req, reply) => {
         const resultado = await pool.query('INSERT INTO CATEGORIA (NOME) VALUES ($1) RETURNING *', [nome])
         reply.status(200).send(resultado.rows[0])
     }catch(e){
+        reply.status(500).send({ error: e.message })
+    }
+})
+
+// deletar
+
+server.delete('/categorias/:id', async (req, reply) => {
+    const id = req.params.id
+
+    try {
+        await pool.query('DELETE FROM CATEGORIA WHERE id=$1', [id])
+        reply.send({ message: 'Categoria deletada '})
+    } catch(e) {
+        reply.send({ error: e.message })
+    }
+})
+
+// editar
+
+server.put('/categorias/:id', async (req, reply) => {
+    const { nome } = req.body
+    const id = req.params.id
+
+    try {
+        const resultado = await pool.query(
+            'UPDATE CATEGORIA SET nome=$1 WHERE id=$2 RETURNING *',
+            [nome, id]
+        )
+        reply.status(200).send(resultado.rows[0])
+    } catch(e) {
         reply.status(500).send({ error: e.message })
     }
 })
